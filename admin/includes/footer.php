1ebc6fd84ba73db72bc191b780702b4d2ec1d434
dips19 <dipakchinchane1@gmail.com>
2024-02-15 00:23:18 +0530

    Intial Commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer" style="background-color: #717A83 ">
+    <div class="pull-right hidden-xs "  style="color:white ; font-size: 13px"  >
+  
+    </div  >
+    <b style="color:white ; font-size: 13px"><strong></strong></b>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer" style="background-color: #717A83 ">
+    <div class="container" style="color:white ; font-size: 13px">
+      <div class="pull-right hidden-xs">
+    
+      </div>
+      <b style="color:white ; font-size: 13px"><strong> </strong></b>
+    </div>
+    <!-- /.container -->
+</footer>
+
